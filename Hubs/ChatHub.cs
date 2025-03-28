ce437a537f397eeef7bdf9d3b2eb64c7039bde16
rishabh.911996 <rishabh.911996@gmail.com>
2025-03-28 11:05:25 -0600

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ComplaintChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string firstName, string lastName, string category, string message, DateTime date)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", firstName, lastName, category, message, date);
+        }
+    }
+}
